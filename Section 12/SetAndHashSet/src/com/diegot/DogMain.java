0bbc94d53047c93ab0b4d333c75e173ef721553c
DiegoT <tellodiego@yahoo.com>
2020-02-02 03:19:06 +0000

    Finish potential issue with equals overriding

---
+++ Section 12/SetAndHashSet/src/com/diegot/DogMain.java
@@ -0,0 +1,14 @@
+package com.diegot;
+
+/**
+ * Created by DiegoT on 02/02/2020.
+ */
+public class DogMain {
+    public static void main(String[] args) {
+        Labrador rover = new Labrador("Rover");
+        Dog rover2 = new Dog("Rover");
+
+        System.out.println(rover2.equals(rover));
+        System.out.println(rover.equals(rover2));
+    }
+}
---
+++ Section 12/SetAndHashSet/src/com/diegot/Labrador.java
@@ -0,0 +1,24 @@
+package com.diegot;
+
+/**
+ * Created by DiegoT on 02/02/2020.
+ */
+public class Labrador extends Dog {
+    public Labrador(String name) {
+        super(name);
+    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//
+//        if (obj instanceof Labrador) {
+//            String objName = ((Labrador) obj).getName();
+//            return this.getName().equals(objName);
+//        }
+//
+//        return false;
+//    }
+}
